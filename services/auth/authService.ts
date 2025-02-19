0662193539460d649bcf30f327678419091db672
Vladimir Pinera <vladimir@danngos.com>
2025-02-19 10:28:30 -0500

    feat(auth): Implement user authentication methods and types

--- services/auth/authService.ts
+++ services/auth/authService.ts
@@ -1,4 +1,5 @@
 import { UserRespository } from "@/repository/auth/userRepository";
+import { LoginRequest, LoginResponse, RegisterRequest, User } from "@/types/auth";
 
 export class TaskService {
     private userRepository: UserRespository;
@@ -7,7 +8,19 @@ export class TaskService {
         this.userRepository = userService;
     }
 
-    async login(formLoginData: any): Promise<any> {
+    async login(formLoginData: LoginRequest): Promise<LoginResponse> {
         return await this.userRepository.login(formLoginData)
+    }
+
+    async register(formSignupData: RegisterRequest): Promise<any> {
+        return await this.userRepository.register(formSignupData)
+    }
+
+    async users(): Promise<User[]> {
+        return await this.userRepository.users()
+    }
+
+    async user(id: string): Promise<User> {
+        return await this.userRepository.user(id)
     }
 }
